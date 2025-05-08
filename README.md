85779bff7f8437daec344200811b36af7c5af2e4
Peter Donker <peter@bring2mind.net>
2025-05-08 15:14:13 +0200

    Add Readme

---
+++ README.md
@@ -0,0 +1,8 @@
+# DNN MVC 2 module solution
+
+This is a test solution that generates 2 MVC modules (Demo1 and Demo2) that can be stuck on a DNN page.
+You can use it to see what happens when you click on details on one module vs another.
+
+The intention of the project is to be a test module for the [new DNN MVC pipeline](https://github.com/DNN-Connect/Dnn.Platform/tree/MVCPL) some of use
+are working on.
+
--- watch.js
+++ watch.js
@@ -4,7 +4,8 @@ const chokidar = require("chokidar"),
   deepmerge = require("deepmerge");
 
 var pkg = require("./package.json"),
-  pkgDemo1 = require("./Server/Demo1/dnn.json");
+  pkgDemo1 = require("./Server/Demo1/dnn.json"),
+  pkgDemo2 = require("./Server/Demo2/dnn.json");
 if (fs.existsSync("./local.json")) {
   var local = require("./local.json");
   pkg = deepmerge(pkg, local);
@@ -27,43 +28,56 @@ function copy(start, srcRelativePath, destDir) {
     });
 }
 
-var ignore = [
-  /obj/,
-  /bin/,
-  "**/*.{vb,cs,suo,user}",
-  "**/dnn.json",
-  "**/*.??proj",
-  "**/web*.config",
-  "**/packages.config",
-  "**/.*"
+const ignores = [
+  "dnn.json",
+  ".cs",
+  "dnn.json",
+  "launchSettings.json",
+  ".csproj",
+  ".vbproj",
+  ".LICENSE.txt",
 ];
 
+const ignoreFunc = (filePath) => {
+  if (filePath.includes("/obj/") || filePath.includes("/bin/")) {
+    return true;
+  }
+  if (ignores.some(ignore => filePath.endsWith(ignore))) {
+    return true;
+  }
+  return false;
+};
+
 const allDlls = pkgDemo1.pathsAndFiles.assemblies
-  // .concat(pkgDemo1.pathsAndFiles.assemblies)
-  .map(dll => "bin/" + dll);
+  .concat(pkgDemo2.pathsAndFiles.assemblies)
+  .map((dll) => "bin/" + dll);
 
 const watcher = (src, dest) =>
   chokidar
     .watch(src, {
-      ignored: ignore,
-      persistent: true
+      ignored: ignoreFunc,
+      persistent: true,
     })
-    .on("add", path => copy(src, path, dest))
-    .on("change", path => copy(src, path, dest));
+    .on("add", (path) => copy(src, path, dest))
+    .on("change", (path) => copy(src, path, dest));
 // Todo: delete events?
 
 // Initialize watchers.
 const Demo1Watcher = watcher(
   "Server/Demo1",
-  pkg.dnn.pathsAndFiles.devSitePath +
-    "\\DesktopModules\\MVC\\Bring2mind\\Demo1"
+  pkg.dnn.pathsAndFiles.devSitePath + "\\DesktopModules\\MVC\\Bring2mind\\Demo1"
+);
+
+const Demo2Watcher = watcher(
+  "Server/Demo2",
+  pkg.dnn.pathsAndFiles.devSitePath + "\\DesktopModules\\MVC\\Bring2mind\\Demo2"
 );
 
 const DllWatcher = chokidar
   .watch(allDlls, {
-    persistent: true
+    persistent: true,
   })
-  .on("add", path => {
+  .on("add", (path) => {
     copy("bin", path, pkg.dnn.pathsAndFiles.devSitePath + "\\bin");
     copy(
       "bin",
@@ -71,7 +85,7 @@ const DllWatcher = chokidar
       pkg.dnn.pathsAndFiles.devSitePath + "\\bin"
     );
   })
-  .on("change", path => {
+  .on("change", (path) => {
     copy("bin", path, pkg.dnn.pathsAndFiles.devSitePath + "\\bin");
     copy(
       "bin",
